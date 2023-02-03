043eb85bfda79d819d458048ce27d37fb257440b
IceNinja <theiceninjalng@gmail.com>
2023-02-03 14:03:45 +0200

    Finishing the game

--- src/main/java/net/theiceninja/deathshuffle/utils/ColorUtil.java
+++ src/main/java/net/theiceninja/deathshuffle/utils/ColorUtil.java
@@ -8,14 +8,13 @@ public class ColorUtil {
         final String WITH_DELIMITER = "((?<=%1$s)|(?=%1$s))";
 
         String[] texts = text.split(String.format(WITH_DELIMITER, "&"));
-
         StringBuilder finalText = new StringBuilder();
 
         for (int i = 0; i < texts.length; i++) {
             if (texts[i].equalsIgnoreCase("&")) {
                 i++;
                 if (texts[i].charAt(0) == '#') {
-                    finalText.append(net.md_5.bungee.api.ChatColor.of(texts[i].substring(0, 7)) + texts[i].substring(7));
+                    finalText.append(ChatColor.of(texts[i].substring(0, 7))).append(texts[i].substring(7));
                 } else {
                     finalText.append(ChatColor.translateAlternateColorCodes('&', "&" + texts[i]));
                 }
